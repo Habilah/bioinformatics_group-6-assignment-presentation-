d7548362cdcea42850b24365cbe1126fff82e041
@Habila <emmanuelhabuh@gmail.com>
2026-03-12 20:56:08 +0100

    Update Vasty_emmanuel.pl
    Signed-off-by: @Habila <emmanuelhabuh@gmail.com>

--- Scripts/Vasty_emmanuel.pl
+++ Scripts/Vasty_emmanuel.pl
@@ -1,1 +1,4 @@
-print "vasty Emmanuel, emmanuelvasty93@gmail.com,@vastyemmanuel,transcriptomics\n";
+print "Vastly Emmanuel\n";
+print "emmanuelvasty93\@gmail.com\n";
+print "\@vastlyemmanuel\n";
+print "Transcriptomics\n";

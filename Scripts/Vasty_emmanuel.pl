30de6cc7a9a9f78a040d8b486fbbdcf8438df225
@Habila <emmanuelhabuh@gmail.com>
2026-03-13 11:26:33 +0100

    Condense personal information into a single print statement
    Signed-off-by: @Habila <emmanuelhabuh@gmail.com>

--- Scripts/Vasty_emmanuel.pl
+++ Scripts/Vasty_emmanuel.pl
@@ -1,4 +1,1 @@
-print "Vastly Emmanuel\n";
-print "emmanuelvasty93\@gmail.com\n";
-print "\@vastlyemmanuel\n";
-print "Transcriptomics\n";
+print "Vastly Emmanuel,emmanuelvasty93@gmail.com,@vastyemmanuel,transcriptomics,pl\n"

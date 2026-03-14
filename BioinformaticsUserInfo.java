1712ba90d42eb3bd4283d67ae4724db4be242dc7
ibrahimsaidu102002-a11y <ibrahimsaidu102002@gmail.com>
2026-03-14 08:05:22 +0100

    Create BioinformaticsUserInfo.java
    Signed-off-by: ibrahimsaidu102002-a11y <ibrahimsaidu102002@gmail.com>

---
+++ BioinformaticsUserInfo.java
@@ -0,0 +1,9 @@
+public class IbrahimMemberInfo {
+    public static void main(String[] args) {
+        // Print required information in specified order
+        System.out.println("IBRAHIM SA'ID");
+        System.out.println("ibrahimsaidu102002@gmail.com");
+        System.out.println("@ibrahimsaidu102002");
+        System.out.println("DNA analysis");
+    }
+}
